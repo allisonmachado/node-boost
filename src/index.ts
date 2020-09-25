5fed3cca0430b766d499b8c74a9e406a869cd7d8
Allison Machado Gonçalves <aallison93@gmail.com>
2020-09-25 13:05:12 +0100

    Organizing app index structure

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,31 @@
-import { Environment } from "./Environment";
-
-import express from "express";
+/* Load environment configuration */
 import dotenv from "dotenv";
 dotenv.config();
 
+/* Import express for web interface */
+import express from "express";
+
+/* Import application definitions */
 import { Logger } from "./Logger";
+import { Environment } from "./Environment";
 import { UserController } from "./controllers/User";
 import { UserRepository } from "./data-source/repositories/UserRepository";
 import { UserService } from "./business/UserService";
 
 const app = express();
-const port = Environment.getPort();
-const logger = new Logger("Main");
 
-app.listen(port, () => {
-    logger.info(`App listening at ${port}`);
-    logger.info(`App running in ${Environment.getLocation()} environment`);
-});
-
-
-/* data access layer: */
+/* init data access layer: */
 const userRepository = new UserRepository();
 
-/* business logic definition: */
+/* init business logic definition: */
 const userService = new UserService(userRepository);
 
-/* application api: */
+/* init application api: */
 new UserController(app, userService);
+
+/* listen */
+app.listen(Environment.getPort(), () => {
+    const logger = new Logger("index");
+    logger.info(`App listening at ${Environment.getPort()}`);
+    logger.info(`App running in ${Environment.getLocation()} environment`);
+});
