69c980e033daa2aec5482424ae5d5d8d25c9b9d0
Allison Machado Gonçalves <aallison93@gmail.com>
2020-09-25 23:58:32 +0100

    Create user on repository layer

--- src/SharedLibrary.ts
+++ src/SharedLibrary.ts
@@ -4,5 +4,11 @@ import * as _ from "lodash";
  * Should expose only static pure functions.
  */
 export class SharedLibrary {
-  
+  public static async delay(milliseconds: number): Promise<void> {
+      return new Promise((resolve) => {
+          setTimeout(() => {
+              resolve();
+          }, milliseconds);
+      })
+  }
 }
--- src/data/repositories/UserRepository.ts
+++ src/data/repositories/UserRepository.ts
@@ -10,7 +10,14 @@ export class UserRepository extends RespositoryTemplate {
         super(connection);
         this.llogger.debug(`initialized`);
     }
-    
+
+    public async create(name, surname, email, password): Promise<number> {
+        const sql = "INSERT INTO `simple_db`.`user` (`name`, `surname`, `email`, `password`) "
+            + "VALUES (?, ?, ?, ?)";
+        const data = await this.query(sql, [name, surname, email, password]);
+        return data.insertId;
+    }
+
     public async findById(id: number): Promise<UserEntity[]> {
         const data = await this.query("SELECT * FROM simple_db.user WHERE id = ?", [id]);
         return data.map((d: any) => new UserEntity(d.id, d.name, d.surname, d.email, d.password));
