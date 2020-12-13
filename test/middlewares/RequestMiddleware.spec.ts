d6281222a4cb38c08585f42635331f4f7f6b6402
Allison Machado Gonçalves <allison.goncalves.ext@siemens.com>
2020-12-13 16:03:41 +0000

    Testing request middleware

--- test/middlewares/RequestMiddleware.spec.ts
+++ test/middlewares/RequestMiddleware.spec.ts
@@ -12,7 +12,7 @@ describe("Request Middleware", () => {
     describe("global error handling", async () => {
         it("for body-parser json syntax error should return 400 status code", async () => {
             const error = new SyntaxError(`unexpected string at JSON in position...`);
-            
+
             // @ts-ignore
             const middleware = new RequestMiddleware(logger);
             const response = {
@@ -33,7 +33,7 @@ describe("Request Middleware", () => {
 
         it("for unexpected errors should return 500 status code", async () => {
             const error = new Error();
-            
+
             // @ts-ignore
             const middleware = new RequestMiddleware(logger);
             const response = {
@@ -50,6 +50,35 @@ describe("Request Middleware", () => {
             expect(response.status.calledOnce).to.be.true;
             expect(response.status.calledWithExactly(500)).to.be.true;
             expect(next.notCalled).to.be.true;
+        });
+    });
+
+    describe("request logging", async () => {
+        it("should log method, url, status code and duration given a request response", async () => {
+            const next = sinon.spy();
+            const req = {
+                method: 'GET',
+                url: 'http://myapp.com'
+            };
+            const res = {
+                statusCode: 200,
+                on(event: string, callback: () => void) {
+                    expect(event).to.equal("finish");
+                    callback();
+                }
+            };
+            const logger: ILogger = new EmptyLogger();
+            logger.info = sinon.spy();
+            const middleware = new RequestMiddleware(logger);
+
+            // @ts-ignore
+            middleware.log(req, res, next);
+
+            // @ts-ignore
+            expect(logger.info.calledOnce).to.be.true;
+
+            // @ts-ignore
+            expect(logger.info.firstCall.lastArg).to.equal('GET:http://myapp.com 200 - 0ms');
         });
     });
 });
